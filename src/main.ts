7d753da55cc8c6815d47b70fea4125cc68bffad4
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-26 11:46:20 +0000

    feat: swagger & swagger ui setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { ApiConfig } from "./config/api"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import { INestApplication } from "@nestjs/common"
+import { EnvConfig } from "./config/env"
+
+function setupSwagger(app: INestApplication) {
+    const { environment } = app.get(EnvConfig)
+
+    if (environment !== "development") {
+        return
+    }
+
+    const config = new DocumentBuilder()
+        .setTitle("Coaster API")
+        .setVersion("1.0")
+        .addTag("coasters")
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("api", app, document)
+}
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    setupSwagger(app)
     const { port } = app.get(ApiConfig)
     await app.listen(port)
 }
