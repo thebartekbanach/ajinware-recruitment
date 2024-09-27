47a225d59ca5db45f66184af8a9560b8379b0d63
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-27 09:21:58 +0000

    fix: correctly check for db file existence

--- src/common/database/json-file-db-context.service.ts
+++ src/common/database/json-file-db-context.service.ts
@@ -1,18 +1,38 @@
-import { readFile, writeFile, stat } from "fs/promises"
+import { readFile, writeFile, stat, access, constants } from "fs/promises"
 
 export abstract class JsonFileDbContextService<TEntity> {
     constructor(protected readonly filePath: string) {}
 
-    async read(): Promise<TEntity[]> {
-        const fileStat = await stat(this.filePath)
-        const fileExists = fileStat.isFile()
+    private async checkDbPathCorrectness() {
+        try {
+            await access(this.filePath, constants.W_OK)
+            const info = await stat(this.filePath)
+            return info.isFile()
+        } catch (error) {
+            throw new Error(
+                `Cannot write to the database file (${this.filePath}): ${error.message}`,
+            )
+        }
+    }
 
+    async read(): Promise<TEntity[]> {
+        const fileExists = await this.checkDbPathCorrectness()
         if (!fileExists) {
             return []
         }
 
         const fileContents = await readFile(this.filePath, { encoding: "utf8" })
-        return JSON.parse(fileContents)
+        if (fileContents.length === 0) {
+            return []
+        }
+
+        try {
+            return JSON.parse(fileContents)
+        } catch (error) {
+            throw new Error(
+                `Cannot parse the database file (${this.filePath}): ${error.message}`,
+            )
+        }
     }
 
     async save(data: TEntity[]): Promise<void> {
